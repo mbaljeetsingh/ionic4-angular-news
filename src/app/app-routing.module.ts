09f705679ec9332d72714d507558348cfc0c5a9a
mbaljeetsingh <bsinghs.007@gmail.com>
2018-07-28 11:14:29 +0530

    add news & news-single page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'news', loadChildren: './news/news.module#NewsPageModule' },
+  { path: 'news-single', loadChildren: './news-single/news-single.module#NewsSinglePageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
